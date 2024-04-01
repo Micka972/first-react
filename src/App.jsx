1a8ea8d20d81ee6d63e6a205fc9770b188ebfed9
Micka972 <dem972md@gmail.com>
2024-04-01 18:05:46 +0200

    passer des props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
     <>
       <MyTitle />
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </>
   )
 }
