e37d6a171fa560e5ced54ad07fe4515931a6e764
Micka972 <dem972md@gmail.com>
2024-04-07 18:15:41 +0200

    validation pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,40 @@ import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickIncrement = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1)
+    }
+  }
+
+  const handleClickDecrement = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1)
+    }
+  }
+
+
   const pokemonList = [
     {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
@@ -18,9 +47,17 @@ function App() {
   return (
     <>
       <MyTitle />
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <div>
+        {pokemonIndex > 0 && (
+          <button onClick={handleClickDecrement}>Précédent</button>
+        )}
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button onClick={handleClickIncrement}>Suivant</button>
+        )}
+      </div>
     </>
   )
-}
+};
 
 export default App
